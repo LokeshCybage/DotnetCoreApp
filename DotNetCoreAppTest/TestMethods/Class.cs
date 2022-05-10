ed123401d7c4820504105b76a274c2117ff0fbbc
Lokesh Abhichandani <lokesha@cybage.com>
2022-05-10 12:50:05 +0530

    Dot net core project added with tests

---
+++ DotNetCoreAppTest/TestMethods/Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DotNetCoreApp.BusinessLogic;
+using NUnit.Framework;
+
+namespace DotNetCoreAppTest.TestMethods
+{
+    class Class
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestSum()
+        {
+            var cal = new MathClass();
+            int result = cal.Sum(2, 3);
+            Assert.IsTrue(result == 5);
+        }
+    }
+}
