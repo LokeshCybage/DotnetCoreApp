6c11413ab1edb398f4e937d447578c7b77ee120f
LokeshCybage <147140522+LokeshCybage@users.noreply.github.com>
2025-04-08 17:44:05 +0000

    upgraded app from .net 2.1 to .net 8

--- DotNetCoreApp/Startup.cs
+++ DotNetCoreApp/Startup.cs
@@ -25,11 +25,11 @@ namespace DotNetCoreApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
